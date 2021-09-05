3e5e1b5d9a2ca1c0663fae53a23a3a1c4cfa0399
Surojit Saha <sahasurojit06@gmail.com>
2021-09-05 19:47:16 +0530

    Remove console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
-console.log('Client side javascript file is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
-// message1.textContent = 'From javascript'
+
 
 const message2 = document.querySelector('#message-2')
 
@@ -15,13 +13,10 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error){
-            // console.log(data.error)
             message1.textContent = ""
             message2.textContent = data.error
         }
         else{
-            // console.log(data.address)
-            // console.log(data.forecast)
             message1.textContent = data.address
             message2.textContent = data.forecast
         }
